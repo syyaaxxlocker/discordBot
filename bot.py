19306a78bbad803e00fa9a649e6f473a6d47b716
syyaaxxlocker <chocomoorebr@gmail.com>
2025-08-14 18:07:11 +0900

    First commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+TOKEN: str = os.getenv('TOKEN')
+
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs/{filename[:-3]}')
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
